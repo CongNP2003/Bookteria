1a78916ac48c7af18337a64ea66e750518eecc6c
Cong <21111062@gmail.com>
2025-06-03 21:31:07 +0700

    jwt token  and base profile ( create, get )

---
+++ bookteria/profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
@@ -0,0 +1,31 @@
+package com.devteria.profile.entity;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+import java.time.LocalDate;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Node("user_profile")
+public class UserProfile {
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    String id;
+
+    @Property("user_id")
+    String userId; // này là id ở service có id của user
+
+    String firstName;
+    String lastName;
+    LocalDate dob;
+    String city;
+
+}
