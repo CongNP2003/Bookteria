df5bf6300231cbc423ad49e96a24fdc108e06a5a
Cong <21111062@gmail.com>
2025-08-17 14:43:12 +0700

    Áp dụng Map/Dictionary-Based Strategy design pattern

--- bookteria/post-service/src/main/java/com/devteria/post/service/PostService.java
+++ bookteria/post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.devteria.post.service;
 
+import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.entity.Post;
@@ -8,6 +9,9 @@ import com.devteria.post.repository.PostRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -20,7 +24,7 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PostService {
     PostMapper postMapper;
-
+    DateTimeFormatter dateTimeFormatter;
     PostRepository postRepository;
 
     public PostResponse createPost(PostRequest request)  {
@@ -38,13 +42,33 @@ public class PostService {
         return postMapper.toPostResponse(result);
     }
 
-    public List<PostResponse> getMyPost() {
+    public PageResponse<PostResponse> getMyPost(int page, int size) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        Sort sort = Sort.by("createdDate").descending();
+
+        Pageable pageable = PageRequest.of(page - 1, size, sort);
         String userId = authentication.getName();
-        return postRepository.findAllByuserId(userId)
-                .stream()
-                .map(postMapper::toPostResponse)
-                .toList();
+
+        var pageData = postRepository.findAllByuserId(userId, pageable);
+
+        var postList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
+//        return postRepository.findAllByuserId(userId)
+//                .stream()
+//                .map(postMapper::toPostResponse)
+//                .toList();
+
+        return PageResponse.<PostResponse>builder()
+                .currenPage(page)
+                .totalPages(pageData.getTotalPages())
+                .totalElements(pageData.getTotalElements())
+                .pageSize(pageData.getSize())
+                .data(postList)
+//                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .build();
     }
 }
